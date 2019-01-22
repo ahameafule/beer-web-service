0dba54032919a161766d2caf06219705ae5e5fa1
Unknown <j.ahameafule@gmx.ch>
2019-01-22 14:13:37 +0100

    initial project

---
+++ src/ch/nyp/Beer.java
@@ -0,0 +1,15 @@
+package ch.nyp;
+
+public class Beer {
+	
+	private String id;
+	private String name;
+	private String description;
+	private int idStyle;
+	
+	@Override
+	public String toString() {
+		return "Beer [id=" + id + "; name=" + name + "; description=" + description + "; idStyle=" + idStyle + "]";
+	}
+
+}
---
+++ src/ch/nyp/BeerAdmin.java
@@ -0,0 +1,88 @@
+package ch.nyp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BeerAdmin {
+
+	String url = "http://api.brewerydb.com/v2/";
+	String apiKey = "?key=1511d0db4a1d6841481c672455358cff";
+
+	HashMap<Integer, String> beerStyles;
+	
+	public static void main(String[] args) {
+		BeerAdmin ba = new BeerAdmin();
+		ba.loadBeerStyles();
+		ba.printBeerStyles();
+	}
+
+	public void loadBeerStyles() {
+		String urlExtension = "styles/";
+		JSONObject object = getObject(url + urlExtension + apiKey);
+
+		beerStyles = new HashMap<Integer, String>();
+
+		for (int i = 0; i < object.getJSONArray("data").length(); i++) {
+			int id = object.getJSONArray("data").getJSONObject(i).getInt("id");
+			String name = object.getJSONArray("data").getJSONObject(i).getString("name");
+			beerStyles.put(id, name);
+		}
+	}
+
+	public void printBeerStyles() {
+		for (Integer key : beerStyles.keySet()) {
+			System.out.println("Key: " + key + " Value: " + beerStyles.get(key));
+		}
+	}
+
+	public void printBeerStyles(String search) {
+
+	}
+
+	public void getBeerListForStyle(int idStyle) {
+
+	}
+
+	private JSONObject getObject(String link) {
+		URL resource = null;
+		URLConnection connection = null;
+
+		try {
+			resource = new URL(link);
+			connection = resource.openConnection();
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		StringBuilder builder = new StringBuilder();
+		String line;
+
+		try {
+			BufferedReader read = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			while ((line = read.readLine()) != null) {
+				builder.append(line);
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		JSONObject object = null;
+		try {
+			object = new JSONObject(builder.toString());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return object;
+	}
+}
